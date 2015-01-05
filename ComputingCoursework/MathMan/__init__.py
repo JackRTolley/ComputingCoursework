4d1daf56217bc2fdc662daf72cab531ce812cd2e
Jack Tolley <JackRTolley@gmail.com>
2015-01-05 23:57:36 +0000

    Implemented Display Started game Logic

---
+++ ComputingCoursework/MathMan/__init__.py
@@ -0,0 +1,23 @@
+import pygame,sys
+from pygame.locals import *
+
+#Start pygame
+pygame.init()
+
+#PrimaryScreen
+x_resolution = 772
+y_resolution = 676
+DISPLAYSURF = pygame.display.set_mode((x_resolution,y_resolution))
+pygame.display.set_caption("MathMan")
+
+#GameLogic
+
+game_state = "splash"
+
+#Main Game Loop
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit
+    
