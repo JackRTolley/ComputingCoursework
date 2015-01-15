80d0dc2ac89d9b029a189e30059841a1e2129d84
Jack Tolley <JackRTolley@gmail.com>
2015-01-15 00:18:38 +0000

    finished

---
+++ ComputingCoursework/MathMan/game_logic/event_handling.py
@@ -0,0 +1,36 @@
+import pygame,math_man,timer_logic,checks,enemies
+from MathMan import variables
+
+my_hero = math_man.MathMan()
+ghosts  = [enemies.Ghost((13,11)),enemies.Ghost((16,11)),enemies.Ghost((13,16)),enemies.Ghost((16,16))]
+my_hero.get_position()
+
+def event_handling_on_draw():
+    
+    
+    for i in ghosts:
+        i.start_up()
+        i.spawn()
+        i.movement()
+    
+        
+    
+    variables.math_man_buff = my_hero.buff   
+    timer_logic.handle_buff_timer()
+    timer_logic.handle_fruit_timer()
+    timer_logic.handle_ghost_timer()
+    my_hero.get_position()
+    my_hero.move()
+    
+    if variables.math_man_buff:
+        my_hero.update_buff()
+    if checks.level_complete():
+        variables.game_state = "level_complete"
+    elif checks.end_game():
+        variables.game_state = "game_over"
+        
+    
+    
+def event_handling_on_action(event):
+    my_hero.get_direction(event) 
+    
---
+++ ComputingCoursework/MathMan/variables.py
@@ -0,0 +1,65 @@
+
+"""Stores the key variables of the game for easy editing/fetching"""
+
+
+
+#Pygame structure
+x_resolution = 772
+y_resolution = 676
+
+#Question system
+difficulty_setting = "easy"
+
+#Game variables
+paused = False
+game_state = "splash"
+speed_setting = "4"
+score         = 0
+lives         = 3
+math_man_buff = False
+buff_timer    = 0
+fruit_timer   = int(speed_setting) * 60
+ghost_timer   = 0
+ghost_spawn   = False
+
+# Questions
+question_displayed = False
+questions = []
+
+
+
+# Grid
+grid = [['BDR','B_H','B_H','B_H','B_H','B_H','B_H','B_H','B_H','B_H','B_H','B_H','B_H','BDL','BDR','B_H','B_H','B_H','B_H','B_H','B_H','B_H','B_H','B_H','B_H','B_H','B_H','BDL'],
+        ['B_V','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','B_V','B_V','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','B_V'],
+        ['B_V','P_N','BDR','B_H','B_H','BDL','P_N','BDR','B_H','B_H','B_H','BDL','P_N','B_V','B_V','P_N','BDR','B_H','B_H','B_H','BDL','P_N','BDR','B_H','B_H','BDL','P_N','B_V'],
+        ['B_V','P_B','B_V','NON','NON','B_V','P_N','B_V','NON','NON','NON','B_V','P_N','B_V','B_V','P_N','B_V','NON','NON','NON','B_V','P_N','B_V','NON','NON','B_V','P_B','B_V'],
+        ['B_V','P_N','BUR','B_H','B_H','BUL','P_N','BUR','B_H','B_H','B_H','BUL','P_N','BUR','BUL','P_N','BUR','B_H','B_H','B_H','BUL','P_N','BUR','B_H','B_H','BUL','P_N','B_V'],
+        ['B_V','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','B_V'],
+        ['B_V','P_N','BDR','B_H','B_H','BDL','P_N','BDR','BDL','P_N','BDR','B_H','B_H','B_H','B_H','B_H','B_H','BDL','P_N','BDR','BDL','P_N','BDR','B_H','B_H','BDL','P_N','B_V'],
+        ['B_V','P_N','BUR','B_H','B_H','BUL','P_N','B_V','B_V','P_N','BUR','B_H','B_H','BDL','BDR','B_H','B_H','BUL','P_N','B_V','B_V','P_N','BUR','B_H','B_H','BUL','P_N','B_V'],
+        ['B_V','P_N','P_N','P_N','P_N','P_N','P_N','B_V','B_V','P_N','P_N','P_N','P_N','B_V','B_V','P_N','P_N','P_N','P_N','B_V','B_V','P_N','P_N','P_N','P_N','P_N','P_N','B_V'],
+        ['BUR','B_H','B_H','B_H','B_H','BDL','P_N','B_V','BUR','B_H','B_H','BDL','NON','B_V','B_V','NON','BDR','B_H','B_H','BUL','B_V','P_N','BDR','B_H','B_H','B_H','B_H','BUL'],
+        ['NON','NON','NON','NON','NON','B_V','P_N','B_V','BDR','B_H','B_H','BUL','NON','BUR','BUL','NON','BUR','B_H','B_H','BDL','B_V','P_N','B_V','NON','NON','NON','NON','NON'],
+        ['NON','NON','NON','NON','NON','B_V','P_N','B_V','B_V','NON','NON','NON','NON','NON','NON','NON','NON','NON','NON','B_V','B_V','P_N','B_V','NON','NON','NON','NON','NON'],
+        ['NON','NON','NON','NON','NON','B_V','P_N','B_V','B_V','NON','BDR','B_H','B_H','NON','NON','B_H','B_H','BDL','NON','B_V','B_V','P_N','B_V','NON','NON','NON','NON','NON'],
+        ['B_H','B_H','B_H','B_H','B_H','BUL','P_N','BUR','BUL','NON','B_V','NON','NON','NON','NON','NON','NON','B_V','NON','BUR','BUL','P_N','BUR','B_H','B_H','B_H','B_H','B_H'],
+        ['NON','NON','NON','NON','NON','NON','P_N','NON','NON','NON','B_V','NON','NON','NON','NON','NON','NON','B_V','NON','NON','NON','P_N','NON','NON','NON','NON','NON','NON'],
+        ['NON','NON','NON','NON','NON','NON','P_N','NON','NON','NON','B_V','NON','NON','NON','NON','NON','NON','B_V','NON','NON','NON','P_N','NON','NON','NON','NON','NON','NON'],
+        ['B_H','B_H','B_H','B_H','B_H','BDL','P_N','BDR','BDL','NON','B_V','NON','NON','NON','NON','NON','NON','B_V','NON','BDR','BDL','P_N','BDR','B_H','B_H','B_H','B_H','B_H'],
+        ['NON','NON','NON','NON','NON','B_V','P_N','B_V','B_V','NON','BUR','B_H','B_H','B_H','B_H','B_H','B_H','BUL','NON','B_V','B_V','P_N','B_V','NON','NON','NON','NON','NON'],
+        ['NON','NON','NON','NON','NON','B_V','P_N','B_V','B_V','NON','NON','NON','NON','NON','NON','NON','NON','NON','NON','B_V','B_V','P_N','B_V','NON','NON','NON','NON','NON'],
+        ['NON','NON','NON','NON','NON','B_V','P_N','B_V','B_V','NON','BDR','B_H','B_H','B_H','B_H','B_H','B_H','BDL','NON','B_V','B_V','P_N','B_V','NON','NON','NON','NON','NON'],
+        ['BDR','B_H','B_H','B_H','B_H','BUL','P_N','BUR','BUL','NON','BUR','B_H','B_H','BDL','BDR','B_H','B_H','BUL','NON','BUR','BUL','P_N','BUR','B_H','B_H','B_H','B_H','BDL'],
+        ['B_V','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','B_V','B_V','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','B_V'],
+        ['B_V','P_N','BDR','B_H','B_H','BDL','P_N','BDR','B_H','B_H','B_H','BDL','P_N','B_V','B_V','P_N','BDR','B_H','B_H','B_H','BDL','P_N','BDR','B_H','B_H','BDL','P_N','B_V'],
+        ['B_V','P_N','BUR','B_H','BDL','B_V','P_N','BUR','B_H','B_H','B_H','BUL','P_N','BUR','BUL','P_N','BUR','B_H','B_H','B_H','BUL','P_N','B_V','BDR','B_H','BUL','P_N','B_V'],
+        ['B_V','P_B','P_N','P_N','B_V','B_V','P_N','P_N','P_N','P_N','P_N','P_N','P_N','M_M','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','B_V','B_V','P_N','P_N','P_B','B_V'],
+        ['BUR','B_H','BDL','P_N','B_V','B_V','P_N','BDR','BDL','P_N','BDR','B_H','B_H','B_H','B_H','B_H','B_H','BDL','P_N','BDR','BDL','P_N','B_V','B_V','P_N','BDR','B_H','BUL'],
+        ['BDR','B_H','BUL','P_N','BUR','BUL','P_N','B_V','B_V','P_N','BUR','B_H','B_H','BDL','BDR','B_H','B_H','BUL','P_N','B_V','B_V','P_N','BUR','BUL','P_N','BUR','B_H','BDL'],
+        ['B_V','P_N','P_N','P_N','P_N','P_N','P_N','B_V','B_V','P_N','P_N','P_N','P_N','B_V','B_V','P_N','P_N','P_N','P_N','B_V','B_V','P_N','P_N','P_N','P_N','P_N','P_N','B_V'],
+        ['B_V','P_N','BDR','B_H','B_H','B_H','B_H','BUL','BUR','B_H','B_H','BDL','P_N','B_V','B_V','P_N','BDR','B_H','B_H','BUL','BUR','B_H','B_H','B_H','B_H','BDL','P_N','B_V'],
+        ['B_V','P_N','BUR','B_H','B_H','B_H','B_H','B_H','B_H','B_H','B_H','BUL','P_N','BUR','BUL','P_N','BUR','B_H','B_H','B_H','B_H','B_H','B_H','B_H','B_H','BUL','P_N','B_V'],
+        ['B_V','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','P_N','B_V'],
+        ['BUR','B_H','B_H','B_H','B_H','B_H','B_H','B_H','B_H','B_H','B_H','B_H','B_H','B_H','B_H','B_H','B_H','B_H','B_H','B_H','B_H','B_H','B_H','B_H','B_H','B_H','B_H','BUL']]
+        
+        
